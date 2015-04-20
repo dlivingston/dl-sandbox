ec33213414dd6fa56101596b2a8570772a6088dc
DonL <donlivingston@gmail.com>
2015-04-19 22:43:27 -0600

    Big commit 2015-04-19

---
+++ config.rb
@@ -0,0 +1,9 @@
+require "susy"
+http_path = "/"
+css_dir = "/css"
+sass_dir = "/sass"
+images_dir = "/images"
+javascripts_dir = "/js"
+relative_assets = true
+line_comments = false
+output_style = :compact
